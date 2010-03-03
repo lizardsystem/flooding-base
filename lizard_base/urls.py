1c3cc4a4607663df2df33013c90618bd740ae467
reinout <reinout@vanrees.org>
2010-03-03 15:08:16 +0000

    Added former in-the-lizard-root urls to base itself and putting base's urls in /service

--- lizard_base/urls.py
+++ lizard_base/urls.py
@@ -1,37 +1,83 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 
+from django.contrib import admin
+
+admin.autodiscover()
+
 
 urlpatterns = patterns(
     '',
-    url(r'^$',
+    # The old lizard.base urls that were previously mounted under /service.
+    # We now mount them there directly.
+    url(r'^service/$',
         'lizard_base.views.service_uberservice',
         name='base_service_uberservice'),
 
-    url(r'^configuration/$',
+    url(r'^service/configuration/$',
         'lizard_base.views.service_get_configurations',
         name='base_service_get_configurations'),
 
-    url(r'^configuration/(?P<configuration_id>\d+)/filter/$',
+    url(r'^service/configuration/(?P<configuration_id>\d+)/filter/$',
         'lizard_base.views.service_get_filters',
         name='base_service_get_filters'),
 
-    url(r'^configuration/(?P<configuration_id>\d+)/filter/(?P<filter_id>[_A-Za-z0-9]*)/location/$',
+    url(r'^service/configuration/(?P<configuration_id>\d+)/filter/(?P<filter_id>[_A-Za-z0-9]*)/location/$',
         'lizard_base.views.service_get_locations',
         name='base_service_get_locations'),
 
-    url(r'^configuration/(?P<configuration_id>\d+)/filter/(?P<filter_id>[_A-Za-z0-9]*)/parameter/$',
+    url(r'^service/configuration/(?P<configuration_id>\d+)/filter/(?P<filter_id>[_A-Za-z0-9]*)/parameter/$',
         'lizard_base.views.service_get_parameters',
         name='base_service_get_parameters'),
 
-    url(r'^configuration/(?P<configuration_id>\d+)/location/(?P<location_id>[_/A-Za-z0-9]*)/$',
+    url(r'^service/configuration/(?P<configuration_id>\d+)/location/(?P<location_id>[_/A-Za-z0-9]*)/$',
         'lizard_base.views.service_get_location',
         name='base_service_get_location'),
 
-    url(r'^configuration/(?P<configuration_id>\d+)/timeseries/$',
+    url(r'^service/configuration/(?P<configuration_id>\d+)/timeseries/$',
         'lizard_base.views.service_get_timeseries',
         name='base_service_get_timeseries'),
+
+    # URLs that were previously defined in the old lizard root urls.py but
+    # that have to do with base.
+
+    url(r'^$',
+        'lizard_base.views.gui',
+        name='root_url'),
+
+    url(r'^get_config.js$',
+        'lizard_base.views.gui_config',
+        name='gui_config'),
+
+    url(r'^get_translated_strings.js$',
+        'lizard_base.views.gui_translated_strings',
+        name='gui_translated_strings'),
+
+    url(r'^admin/$',
+        admin.site.root,
+        name='admin_url'), #manually name admin root
+
+    url(r'^help/$',
+        'lizard_base.views.help',
+        name='help_url'),
+
+    url(r'^userconfiguration/$',
+        'lizard_base.views.userconfiguration',
+        name='userconfiguration_url'),
+
+    url(r'^service/',
+        include('lizard_base.urls')),
+
+    url(r'^base/testdatabase/$',
+        'lizard_base.views.testdatabase_list',
+        name='testdatabase_list',
+        ),
+
+    url(r'^base/testdatabase/(?P<configuration_id>\d+)/$',
+        'lizard_base.views.testdatabase',
+        name='testdatabase_detail'),
     )
+
 
 
 if settings.DEBUG:
