883fb3dbbade62391abffa4f4227185dce9edf92
reinout <reinout@vanrees.org>
2010-03-15 10:24:55 +0000

    Added missing password change urls from old base

--- lizard_base/urls.py
+++ lizard_base/urls.py
@@ -85,6 +85,12 @@ urlpatterns = patterns(
         'django.contrib.auth.views.logout',
         name='logout_url'),
 
+    url(r'^accounts/password_change/$',
+        'django.contrib.auth.views.password_change',
+        name='password_change_url'),
+
+    url(r'^accounts/password_change_done/$',
+        'django.contrib.auth.views.password_change_done'),
 
 
     )
